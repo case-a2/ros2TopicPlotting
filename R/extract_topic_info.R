cbb138f3e3743350142d9e4613cea1b3ee7b14a1
Case <caseashton2@gmail.com>
2023-12-12 06:42:17 -0600

    Touched up the return function on the extract topic method for parsing the .db3 bag file.

--- R/extract_topic_info.R
+++ R/extract_topic_info.R
@@ -3,34 +3,45 @@
 # Input: .db3 file generated from a ROS 2 bag function
 
 # Output: A list of the topic names and message types
-
-#### TESTS ####
 library(RSQLite)
-library(readr)
-
+source()
 
 extract_topic_info <- function(bag_name) {
   sqlite <- dbDriver("SQLite")
 
   ## Connect to bag file to retrieve topic information
-
   file_path_bag <- file.path("vignettes", bag_name, paste0(bag_name, "_0.db3"))
 
+  # Open db connection to read the bag file
   db_info <- dbGetInfo(dbConnect(sqlite, file_path_bag))
   sql_conn <- dbConnect(RSQLite::SQLite(), db_info[["dbname"]])
 
   name = dbGetQuery(sql_conn, "SELECT name FROM topics")
   type = dbGetQuery(sql_conn, "SELECT type FROM topics")
 
+  # Pull the topic name and message type into a list
   topic_information <- list(
-    name = name[[1]],
+    name = name,
     type = type
   )
 
+  # Find the number of topics in the bag file
+  num_topics <- length(topic_information$name)
 
-  return(topic_information)
+  # Loops through the CSV files associated with the bag file
+  for (i in num_topics) {
+    # send to csv function
+    topic_name <- topic_information$name[[1]][i]
+    topic_type <- topic_information$type[[1]][i]
+
+    csv_msg_format(bag_name, topic_name, topic_type)
+  }
+
 
   dbDisconnect(sql_conn)
+
+  # maybe export the new table you made?
+  return(topic_information)
 }
 
 # Example usage:
